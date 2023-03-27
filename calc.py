b0800b655f04be79a111b4b72c2b2e89d36a7548
Seinu <102974491+Seinuve@users.noreply.github.com>
2023-03-27 01:30:47 -0600

    calc.py spacing
    removed a useless import and spaced out my code

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 import os
 
-from time import sleep
 
 nput = ""
 Sum = 0
@@ -8,15 +7,22 @@ Sum = 0
 while(0 == 0):
 
     nput = input()
+    
     if(nput == "q"):
         exit()
+        
     elif(nput ==""):
+        
         Sum = 0
         os.system('cls')
+        
     elif(nput.isdigit()):
+        
         os.system('cls')
         Sum += int(nput)
+        
         print(Sum)
+        
     else:
         pass
 
